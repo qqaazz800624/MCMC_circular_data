2e833b4381f656fccd23745f30a2c6b7b9bd4fe2
D11948002 <d11948002@ntu.edu.tw>
2026-04-10 14:13:17 +0800

    add objectives

---
+++ scripts/objectives.py
@@ -0,0 +1,28 @@
+#%%
+
+import numpy as np
+
+def toy_objective_1(x, g=None, alpha=1.0, beta=0.05):
+    x = np.asarray(x)
+    n = len(x)
+    
+    if g is None:
+        g = np.arange(n, 0, -1)
+    else:
+        g = np.asarray(g)
+        if len(g) != n:
+            raise ValueError(f"The length of weight array g ({len(g)}) must match the length of input array x ({n}).")
+
+    linear_term = np.sum(g * x)
+    
+    interaction_term = 0
+    for i in range(n):
+        indices = [(i + j) % n for j in range(5)]
+        interaction_term += np.prod(x[indices])
+        
+    return alpha * linear_term + beta * interaction_term
+
+
+#%%
+
+
