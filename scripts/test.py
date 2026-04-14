73d3fee963f3f8748dab99160db4c213e7971f41
qqaazz800624 <qqaazz800624@gmail.com>
2026-04-14 16:14:39 +0800

    test a few functions

--- scripts/test.py
+++ scripts/test.py
@@ -1,8 +1,11 @@
 #%%
 
 import numpy as np
+import itertools
 
-np.linspace(0.8, 0.6, num=9)
+elements = np.linspace(0.8, 0.6, num=9).tolist()
+permutations = list(itertools.permutations(elements))
+permutations
 
 #%%
 
