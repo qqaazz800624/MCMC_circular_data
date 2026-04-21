a74cc956cfbb337985a55aa53241fb235d855a44
qqaazz800624 <qqaazz800624@gmao.com>
2026-04-21 18:23:03 +0800

    update

--- commands/run_experiment.sh
+++ commands/run_experiment.sh
@@ -7,7 +7,7 @@ python scripts/run_experiment.py \
     --g 4.9 4.8 4.7 4.6 4.5 4.4 4.3 4.2 4.1 \
     --x 0.8 0.775 0.75 0.725 0.7 0.675 0.65 0.625 0.6 \
     --objective "toy_objective_1" \
-    --proposal "random_swap_proposal" \
+    --proposal "block_pair_exchange_proposal" \
     --alpha 1.0 \
     --beta 10 \
     --tau 5 \
